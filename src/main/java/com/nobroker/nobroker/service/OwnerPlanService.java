9469e9c07000cb57657960e036838cbe10ba1374
91886 <Snehal604@gmail.com>
2024-01-10 00:37:06 +0530

    5th jan 2024

---
+++ src/main/java/com/nobroker/nobroker/service/OwnerPlanService.java
@@ -0,0 +1,7 @@
+package com.nobroker.nobroker.service;
+
+import com.nobroker.nobroker.payload.OwnerPlanDto;
+
+public interface OwnerPlanService {
+    public OwnerPlanDto createOwnerPlans(OwnerPlanDto ownerPlanDto);
+}
