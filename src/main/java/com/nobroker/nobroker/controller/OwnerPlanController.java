6b000eab9cd475712f31d27f70508d6c4ca80849
91886 <Snehal604@gmail.com>
2024-01-11 23:37:29 +0530

    8th jan 2024

--- src/main/java/com/nobroker/nobroker/controller/OwnerPlanController.java
+++ src/main/java/com/nobroker/nobroker/controller/OwnerPlanController.java
@@ -1,22 +1,32 @@
 package com.nobroker.nobroker.controller;
 
 import com.nobroker.nobroker.payload.OwnerPlanDto;
+import com.nobroker.nobroker.service.OwnerPlanService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/ownerPlan")
 public class OwnerPlanController {
-    @Autowired
-    private OwnerPlanController ownerPlanService;
+   @Autowired
+    private OwnerPlanService ownerPlanService;
+
+//    @Autowired
+//    public OwnerPlanController(OwnerPlanService ownerPlanService) {
+//        this.ownerPlanService = ownerPlanService;
+//    }
     @PostMapping
-    public ResponseEntity<OwnerPlanDto> createOwnerPlan(OwnerPlanDto ownerPlanDto){
-        OwnerPlanDto dto= ownerPlanService.createOwnerPlan(ownerPlanDto).getBody();
+    public ResponseEntity<OwnerPlanDto> createOwnerPlan(@RequestBody OwnerPlanDto ownerPlanDto){
+        OwnerPlanDto dto= ownerPlanService.createOwnerPlans(ownerPlanDto);
         return new ResponseEntity<>(dto, HttpStatus.CREATED);
 
+    }
+    @GetMapping
+    public List<OwnerPlanDto> getAllOwnerPlans(){
+        return ownerPlanService.getAllOwnerPlans();
     }
 }
